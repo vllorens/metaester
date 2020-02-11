58818978fb3941d051f135b035faaf142a1d7849
vllorens <vllorens9@gmail.com>
2020-02-11 16:01:51 +0100

    Updated package for compatibility with R 3.5 and above

--- R/makeDataObjects.R
+++ R/makeDataObjects.R
@@ -2,7 +2,7 @@
 # creates data object for the species distribution
 
 speciesDistribution=get(load("inst/extdata/speciesDist.RData"))
-devtools::use_data(speciesDistribution, compress = "xz", internal = TRUE, overwrite = T)
+usethis::use_data(speciesDistribution, compress = "xz", internal = TRUE, overwrite = T)
 
 
 # creates data objects for each of the 5 example genomes
@@ -12,4 +12,4 @@ fprausnitzii=read.fasta("inst/extdata/fprausnitzii.genes.fna",as.string = TRUE)
 rintestinalis=read.fasta("inst/extdata/rintestinalis.genes.fna",as.string = TRUE)
 ljohnsonii=read.fasta("inst/extdata/ljohnsonii.genes.fna",as.string = TRUE)
 bobeum=read.fasta("inst/extdata/bobeum.genes.fna",as.string = TRUE)
-devtools::use_data(efaecalis, fprausnitzii, rintestinalis, ljohnsonii, bobeum, overwrite = T)
+usethis::use_data(efaecalis, fprausnitzii, rintestinalis, ljohnsonii, bobeum, overwrite = T)
